6d96b2ee247df41b0eef4492c8d14ef408994dbf
Gabriel <gabrielruss88@gmail.com>
2016-11-10 16:51:16 -0500

    added files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //runs local dev server
+var open = require('gulp-open'); //open a url in a web browser
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        dist: './dist'
+    }
+}
+
+//start local dev server
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
