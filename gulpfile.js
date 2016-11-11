e58983b5280642b444dcb8827c450c15f5cfd2ed
Gabriel <gabrielruss88@gmail.com>
2016-11-11 06:23:22 -0500

    added browserify, reactify, vinyl-source-stream

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //Runs a local dev server
 var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify'); //bundles our javascript
+var reactify = require('reactify'); //transforms jsx into jsx
+var source = require('vinyl-source-stream'); //use conventional text streams with gulp
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http://localhost',
 	paths: {
-		html: './src/*.html',	
-		dist: './dist'
+		html: './src/*.html',
+		js: './src/**/*.js',	
+		dist: './dist',
+		mainJs: './src/main.js'
 	}
 }
 
@@ -34,8 +39,19 @@ gulp.task('html', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function(){
+	browserify(config.paths.mainJs)
+	.transform(reactify)
+	.bundle()
+	.on('error', console.error.bind(console))
+	.pipe(source('bundle.js'))
+	.pipe(gulp.dest(config.paths.dist + '/scripts'))
+	.pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function(){
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
