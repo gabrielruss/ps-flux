cbf4fbd5848a522c885a06b93e992d54a133fe6f
Gabriel <gabrielruss88@gmail.com>
2016-11-25 17:24:47 -0500

    lknasd

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,21 +1,60 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
 
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function () {
         return {
-            author: { id: '', firstName: '', lastName: '' }
+            author: { id: '', firstName: '', lastName: '' },
+            errors: {}
         };
     },
 
-    setAuthorState: function(event){
+    setAuthorState: function (event) {
         var field = event.target.name;
         var value = event.target.value;
         this.state.author[field] = value;
-        return this.setState({author: this.state.author});
+        return this.setState({ author: this.state.author });
+    },
+
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {};
+
+        if (this.state.author.firstName < 2) {
+            this.state.errors.firstName = 'First name must be at least 2 characters.';
+            formIsValid = false;
+        }
+
+        if (this.state.author.lastName < 2) {
+            this.state.errors.lastName = 'Last name must be at least 2 characters.';
+            formIsValid = false;
+        }
+
+        this.setState({errors: this.state.errors});
+
+        return formIsValid;
+    },
+
+    saveAuthor: function (event) {
+        event.preventDefault();
+
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
+        AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved.');
+        this.transitionTo('authors');
     },
 
     render: function () {
@@ -23,7 +62,8 @@ var ManageAuthorPage = React.createClass({
             <AuthorForm
                 author={this.state.author}
                 onChange={this.setAuthorState}
-                />
+                onSave={this.saveAuthor}
+                errors={this.state.errors} />
         );
     }
 
